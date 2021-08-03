3af3e2249cad2676eb6fff803a0ebeeabe12cd78
Samuel <samuelanjos@academico.ufs.br>
2021-08-03 11:32:59 -0300

    adding the function that takes it from the JS and put in the HTML

--- scripsts.js
+++ scripsts.js
@@ -1,35 +1,43 @@
-const Modal = {
+const Modal = { // obj
     open(){
         //abrir o modal
         //adicionar a class active ao modal
-        document.querySelector('.modal-overlay').classList.add('active')
+        document.querySelector('.modal-overlay')
+        .classList
+        .add('active')
     },
     close(){
     //fechar o Modal
     // remover a class active do modal
-    document.querySelector('.modal-overlay').classList.remove('active')
+    document.querySelector('.modal-overlay')
+    .classList
+    .remove('active')
     }
 }
 
-const transactions = [
+const transactions = [ //array de obj
+
+     //When we work with money we put 2 zeros. Ex: 200 -> 20000
     {
         id=1,
         description='Luz',
-        amount: -50000,
-        date: '23/07/2021'
+        amount: -50000, 
+        date: '23/07/2021',
     },
+
     {
         id=2,
         description='WebSite',
         amount: -500000,
-        date: '23/07/2021'
+        date: '23/07/2021',
     },
+
     {
         id=3,
         description='Internet',
         amount: -20000,
-        date: '23/07/2021'
-    }
+        date: '23/07/2021',
+    },
 ]
 
 const transaction = {
@@ -45,18 +53,49 @@ const transaction = {
 }
 
 const DOM = {
-    innerHTMLTransaction(){
+    
+    transactionsContainer: document.querySelector('#data-table tbody'),
 
+    addTransaction(transaction, index){
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction){
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const amount = Utils.formatCurrency(transaction.amount)
         const html = `
         
-        <tr>
-            <td class="description">Luz</td>
-            <td class="expense">- R$ 500,00</td>
-            <td class="date">23/07/2021</td>
-             <td>
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
             <img src="./image/minus.svg" alt="Remover transação">
             </td>
-        </tr>
         `
+        return html
     }
 }
+
+const Utils = {
+    formatCurrency(value){
+        const signal = Number(value) < 0 ? "-" : ""
+
+        value = String(value).replace(/\D/g, "") //expresao regular see
+        value = Number(value) / 100
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+
+        //console.log(signal + value)
+        return signal + value
+    }
+}
+
+transactions.forEach(function(transaction){
+    DOM.addTransaction(transaction)
+})
